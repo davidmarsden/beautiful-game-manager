582e5eb09373b181ba52a64844d97a7475247c04
davidmarsden <dmarsd@gmail.com>
2026-08-19 12:05:31 +0100

    #240 rely on canonical split squad rule projection

--- public/internal-profile-links.js
+++ public/internal-profile-links.js
@@ -1,6 +1,5 @@
 import './transfer-grace-pending-fix.js';
 import './transfer-history.js';
-import './split-squad-cap-display.js';
 import { openTbgPlayerProfile } from './player-profile.js';
 
 let historyDirectory = null;
