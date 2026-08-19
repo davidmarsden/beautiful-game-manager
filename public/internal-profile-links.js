e7fd29a5d41b05c79a9657cb3b947e5cd2357cfc
davidmarsden <dmarsd@gmail.com>
2026-08-19 21:46:52 +0100

    #240 load unified open market UI

--- public/internal-profile-links.js
+++ public/internal-profile-links.js
@@ -1,5 +1,6 @@
 import './transfer-grace-pending-fix.js';
 import './transfer-history.js';
+import './open-market.js';
 import { openTbgPlayerProfile } from './player-profile.js';
 
 let historyDirectory = null;
