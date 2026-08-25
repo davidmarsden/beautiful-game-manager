46414261fccfbfacafd6d75ef22e0d8da930b6fd
davidmarsden <dmarsd@gmail.com>
2026-08-25 10:36:15 +0100

    #333 load transfer section controller

--- public/internal-profile-links.js
+++ public/internal-profile-links.js
@@ -9,6 +9,7 @@ import './external-market-ui.js';
 import './rating-history-enhancements.js';
 import './transfer-exchange-response-ui.js';
 import './transfer-exchange-direct-controls.js';
+import './transfer-section-cards.js';
 import { openTbgPlayerProfile } from './player-profile.js';
 
 let historyDirectory = null;
