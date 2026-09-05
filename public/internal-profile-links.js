0bd912f01a6d66811ce5841c03af6ade838a257d
davidmarsden <dmarsd@gmail.com>
2026-09-05 10:56:15 +0100

    Load positional versatility UI

--- public/internal-profile-links.js
+++ public/internal-profile-links.js
@@ -10,6 +10,7 @@ import './external-market-ui.js';
 import './rating-history-enhancements.js';
 import './squad-player-statistics.js';
 import './preset-substitutions.js';
+import './position-versatility-loader.js';
 import './alpha-team-ux-fixes.js';
 import './alpha-presentation-fixes.js';
 import './transfer-exchange-response-ui.js';
