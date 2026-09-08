d1fb879b70c30bf3d466f0d4038065d8f7fc875c
davidmarsden <dmarsd@gmail.com>
2026-09-08 13:48:20 +0100

    Load unmanaged-club offer guard

--- public/internal-profile-links.js
+++ public/internal-profile-links.js
@@ -17,6 +17,7 @@ import './transfer-exchange-response-ui.js';
 import './transfer-exchange-direct-controls.js';
 import './transfer-section-cards.js';
 import './global-search.js';
+import './unmanaged-player-offer-guard.js';
 import { openTbgPlayerProfile } from './player-profile.js';
 
 let historyDirectory = null;
