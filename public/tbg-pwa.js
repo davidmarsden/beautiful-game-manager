0b8801b12ae95031004b47f182ea691a3458a6a3
davidmarsden <dmarsd@gmail.com>
2026-09-13 13:57:13 +0100

    Remove redundant PWA registration helper

--- public/tbg-pwa.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/tbg-sw.js', { scope: '/', updateViaCache: 'none' }).catch(() => undefined);
-  });
-}
